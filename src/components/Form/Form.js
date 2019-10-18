5f37b1ce6d6ce93c84333a20e2df45e4a730776d
James Blount IV <j.blount166@gmail.com>
2019-10-18 11:08:38 -0500

    Step1 part1

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            image_url: "",
+            name: "",
+            price: 0
+        }
+    }
+
+    userImageChange = e => {
+        this.setState({image_url: e.target.value});
+    };
+
+    userProductNameChange = e => {
+        this.setState({name: e.target.value});
+    };
+
+    userPriceChange = e => {
+        this.setState({price: e.target.value});
+    };
+
+    resetInputs = e => {
+        e.preventDefault();
+        this.setState({
+            image_url: "",
+            name: "",
+            price: 0
+        });
+        
+        console.log(this.resetInputs);
+    };
+
     render() {
         return (
             <div>
-                
+                <input placeholder="Image URL" onChange={this.userImageChange} />
+                <input placeholder="Product Name" onChange={this.userProductNameChange} />
+                <input placeholder="Product Price" onChange={this.userPriceChange} />
+                <button>Add</button>
+                <input type="button" name="resetInputs" value="Cancel" onClick={this.resetInputs} />
             </div>
         )
     }
