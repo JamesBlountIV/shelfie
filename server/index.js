a29de7e1a9479b7176868c72023880dc788a278e
James Blount IV <j.blount166@gmail.com>
2019-10-18 10:09:30 -0500

    database step

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const massive = require("massive");
+const {CONNECTION_STRING} = process.env;
 
 const app = express();
+
+massive(CONNECTION_STRING).then(dbInstance => {
+    app.set('db', dbInstance);
+}).catch(err => console.log(err));
 
 app.use(express.json());
 
