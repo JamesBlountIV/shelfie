3147761cc41429dc7edbfebe161c88b458038fd8
James Blount IV <j.blount166@gmail.com>
2019-10-18 09:19:20 -0500

    React step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Form from './components/Form/Form';
+import Header from './components/Header/Header';
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Dashboard />
+      <Header />
+      <Form />
     </div>
   );
 }
